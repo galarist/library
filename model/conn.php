89ba4e503b9ad550571a9e9d4d4faee27f068073
Galarist <galamboscristovao@gmail.com>
2021-08-20 04:39:04 +1000

    beautify conn

--- model/conn.php
+++ model/conn.php
@@ -1,16 +1,14 @@
 <?php
 $host = 'localhost';
-$user = 'admin'; //replace with your database username
-$password = 'admin'; //replace with your database password
-$dbname = 'library'; //replace with your database name
+$user = 'admin'; //database username
+$password = 'admin'; //database password
+$dbname = 'library'; //database name
 $dsn = '';
 
-try{
+try {
     $dsn = 'mysql:host='.$host. ';dbname='.$dbname;
-
     $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
+} catch (PDOException $e) {
     echo 'connection failed: '.$e->getMessage();
 }
-?>
