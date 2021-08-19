eae10e5af8962e959790d0d398c15dff6b9f13f7
Galarist <galamboscristovao@gmail.com>
2021-08-20 04:36:19 +1000

    change name order

--- resources/library.sql
+++ resources/library.sql
@@ -6,8 +6,8 @@ USE library;
 CREATE TABLE users (
     userID int NOT NULL AUTO_INCREMENT,
     permission int,
-    lastName varchar(255),
     firstName varchar(255),
+    lastName varchar(255),
     username varchar(255),
     email varchar(255),
     password varchar(255),
@@ -15,7 +15,7 @@ CREATE TABLE users (
 );
 
 INSERT INTO users
-    (userID, permission, lastName, firstName, username, email, password)
+    (userID, permission, firstName, lastName, username, email, password)
 VALUES 
     (1, 1, "John", "Doe", "admin", "admin@example.com", "$2y$12$RSy9zywha1Qlg0ml2fYD4OfnZrESDu2v5h8jOXwNr4dO/HURLF6nO");
 
