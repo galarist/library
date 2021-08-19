788caa39302d6f38bd4b6dfba3fecc218633c247
Galarist <galamboscristovao@gmail.com>
2021-08-20 04:37:44 +1000

    adding names to the new user

--- users/signup.php
+++ users/signup.php
@@ -2,15 +2,13 @@
 //define('BASEPATH', true); //access connection script if you omit this line file will be blank
 require '../model/conn.php'; //require connection script
 
- if(isset($_POST['submit'])){  
-        try {
-            $dsn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-            $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
+ if (isset($_POST['submit'])) {
+     try {
+         $dsn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+         $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
-         $user = $_POST['username'];
-         $user = $_POST['username'];
+         $fName = $_POST['firstName'];
+         $lName = $_POST['lastName'];
          $user = $_POST['username'];
          $email = $_POST['email'];
          $pass = $_POST['password'];
@@ -25,34 +23,29 @@ require '../model/conn.php'; //require connection script
          $stmt->execute();
          $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-         if($row['num'] > 0){
+         if ($row['num'] > 0) {
              echo '<script>alert("Username already exists")</script>';
-        }
-        
-       else{
-
-    $stmt = $dsn->prepare("INSERT INTO users (username, email, password) 
-    VALUES (:username,:email, :password)");
-    $stmt->bindParam(':username', $user);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':password', $pass);
-    
-    
-
-   if($stmt->execute()){
-    echo '<script>alert("New account created.")</script>';
-    //redirect to another page
-    echo '<script>window.location.replace("/library/users/signin.php")</script>';
-     
-   }else{
-       echo '<script>alert("An error occurred")</script>';
-   }
-}
-}catch(PDOException $e){
-    $error = "Error: " . $e->getMessage();
-    echo '<script type="text/javascript">alert("'.$error.'");</script>';
-}
+         } else {
+             $stmt = $dsn->prepare("INSERT INTO users (firstName, lastName, username, email, password) 
+    VALUES (:firstName, :lastName, :username, :email, :password)");
+             $stmt->bindParam(':firstName', $fName);
+             $stmt->bindParam(':lastName', $lName);
+             $stmt->bindParam(':username', $user);
+             $stmt->bindParam(':email', $email);
+             $stmt->bindParam(':password', $pass);
+             if ($stmt->execute()) {
+                 echo '<script>alert("New account created.")</script>';
+                 //redirect to another page
+                 echo '<script>window.location.replace("/library/users/signin.php")</script>';
+             } else {
+                 echo '<script>alert("An error occurred")</script>';
+             }
+         }
+     } catch (PDOException $e) {
+         $error = "Error: " . $e->getMessage();
+         echo '<script type="text/javascript">alert("'.$error.'");</script>';
      }
+ }
 
 ?>
 
