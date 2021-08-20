5f15ae0341b32289e9a20a9e87a9105d79a0ead3
Galarist <galamboscristovao@gmail.com>
2021-08-20 14:30:05 +1000

    Add second user as  default

--- resources/library.sql
+++ resources/library.sql
@@ -17,7 +17,8 @@ CREATE TABLE users (
 INSERT INTO users
     (userID, permission, firstName, lastName, username, email, password)
 VALUES 
-    (1, 1, "John", "Doe", "admin", "admin@example.com", "$2y$12$RSy9zywha1Qlg0ml2fYD4OfnZrESDu2v5h8jOXwNr4dO/HURLF6nO");
+    (1, 1, "John", "Doe", "John", "John@example.com", "$2y$12$RSy9zywha1Qlg0ml2fYD4OfnZrESDu2v5h8jOXwNr4dO/HURLF6nO"),
+    (2, 0, "Jane", "Doe", "Jane", "Jane@example.com", "$2y$12$RSy9zywha1Qlg0ml2fYD4OfnZrESDu2v5h8jOXwNr4dO/HURLF6nO");
 
 /*CREATE TABLE FOR BOOKS*/
 CREATE TABLE books (
