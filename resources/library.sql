80ae6ee799e31ba6b81f40aa6ef8d2d6ede2c4c1
Galarist <galamboscristovao@gmail.com>
2021-08-20 14:51:48 +1000

    Update info

--- resources/library.sql
+++ resources/library.sql
@@ -18,7 +18,7 @@ INSERT INTO users
     (userID, permission, firstName, lastName, username, email, password)
 VALUES 
     (1, 1, "John", "Doe", "John", "John@example.com", "$2y$12$RSy9zywha1Qlg0ml2fYD4OfnZrESDu2v5h8jOXwNr4dO/HURLF6nO"),
-    (2, 0, "Jane", "Doe", "Jane", "Jane@example.com", "$2y$12$RSy9zywha1Qlg0ml2fYD4OfnZrESDu2v5h8jOXwNr4dO/HURLF6nO");
+    (2, 1, "Jane", "Doe", "Jane", "Jane@example.com", "$2y$12$RSy9zywha1Qlg0ml2fYD4OfnZrESDu2v5h8jOXwNr4dO/HURLF6nO");
 
 /*CREATE TABLE FOR BOOKS*/
 CREATE TABLE books (
