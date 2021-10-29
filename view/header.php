f2272f57126711d4b820deb75d2f0a51185e77a6
galarist <galamboscristovao@gmail.com>
2021-10-30 00:14:54 +1000

    fixing header

--- view/header.php
+++ view/header.php
@@ -4,15 +4,14 @@
     <ul>
       <li><a class="active" href="/library">Home</a></li>
       <li><a href="/library/users/signout.php">Sign Out</a></li>
-      <?php echo ($_SESSION["permission"] == 1) ? '<li><a href="/library/users/dashboard.php">Dashboard</a></li>' : '' ?>
+      <?php echo ($_SESSION["permission"] == 1) ? '<li><a href="/library/users/signup.php">Add User</a></li><li><a href="/library/users/dashboard.php">Dashboard</a></li>' : '' ?>
     </ul>
     <?php
   } else {
     ?>
     <ul>
       <li><a class="active" href="/library">Home</a></li>
-      <li><a href="/library/users/signin.php">Sign In</a></li>
-      <li><a href="/library/users/signup.php">Sign Up</a></li>
+      <?php /*If user is not set display Sign In*/ echo (!isset($_SESSION['username'])) ? '<li><a href="/library/users/signin.php">Sign In</a></li>' : '' ?>
     </ul>
     <?php 
   }
